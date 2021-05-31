64a313ecf3c5c369e9befcf56aa2d2827b0c9135
kajal-light <kajalpshelke@gmail.com>
2021-05-31 13:57:55 +0530

    Updated code with AWS deployment details
    Deployed code war on AWS

--- src/main/java/com/kajal/mobile/app/MobileAppWsApplication.java
+++ src/main/java/com/kajal/mobile/app/MobileAppWsApplication.java
@@ -1,7 +1,9 @@
 package com.kajal.mobile.app;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -9,8 +11,16 @@ import com.kajal.mobile.app.ui.appproperti.AppProperties;
 import com.kajal.mobile.app.ui.shared.dto.Spring.SpringApplicationsContext;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
 
+	@Override
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		
+		return application.sources(MobileAppWsApplication.class);
+		
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 		
--- src/main/java/com/kajal/mobile/app/ui/appproperti/AppProperties.java
+++ src/main/java/com/kajal/mobile/app/ui/appproperti/AppProperties.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppProperties {
 
-@Autowired	
-private Environment env;
-public String  getTokenSecret() {
-return env.getProperty("tokenSecret");
-}
+	@Autowired
+	private Environment env;
+
+	public String getTokenSecret() {
+		return env.getProperty("tokenSecret");
+	}
 }
